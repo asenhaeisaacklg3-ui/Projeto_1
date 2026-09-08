24bed262a5e6b28dbc2b2b2ee9b57d7a48e94427
asenhaeisaacklg3-ui <asenhaeisaacklg3@gmail.com>
2026-09-08 13:51:10 -0300

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-ano_de_nascimento = int(input('Digite seu ano de nascimento: '))
-atual = int(input('Digite o ano em que você esta: '))
-idade = atual - ano_de_nascimento
-print('Já que você nasceu em', ano_de_nascimento,'e esta no ano de', atual,'você tem',idade)
